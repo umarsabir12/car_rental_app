93f0f4cb4911de19abcb8359d49b31e0ea2ce67e
umarsabir <umarsabir1997@gmail.com>
2025-10-01 04:18:59 +0500

    remove transactions and booking charts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.2].define(version: 2025_09_30_225417) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
