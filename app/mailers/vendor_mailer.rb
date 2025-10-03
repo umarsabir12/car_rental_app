4055d68dcb9709ce5eb4a4d06ae1d1d943745fcf
umarsabir <umarsabir1997@gmail.com>
2025-10-03 05:48:28 +0500

    Add debugging and fix vendor invite email

--- app/mailers/vendor_mailer.rb
+++ app/mailers/vendor_mailer.rb
@@ -4,7 +4,15 @@ class VendorMailer < ApplicationMailer
   def invite_email(vendor)
     @vendor = vendor
     @login_url = new_vendor_registration_url(token: @vendor.invite_token)
-    mail(to: @vendor.email, subject: 'You have been invited to join WheelsOnRent as a Vendor')
+    
+    Rails.logger.info "VendorMailer: Sending invite email to #{@vendor.email}"
+    Rails.logger.info "VendorMailer: Login URL: #{@login_url}"
+    Rails.logger.info "VendorMailer: From address: #{ApplicationMailer.default[:from]}"
+    
+    mail(
+      to: @vendor.email, 
+      subject: 'You have been invited to join WheelsOnRent as a Vendor'
+    )
   end
 
   def emirates_id_expired_email(vendor)
--- app/models/InvitedVendor.rb
+++ app/models/InvitedVendor.rb
@@ -1,9 +1,17 @@
 class InvitedVendor < ApplicationRecord
   
-    after_create :send_invite_email
+  after_create :send_invite_email
 
-    def send_invite_email
-        self.update(invite_sent: true, invite_token: SecureRandom.hex(10))
-        VendorMailer.invite_email(self).deliver_now
+  def send_invite_email
+    begin
+      self.update(invite_sent: true, invite_token: SecureRandom.hex(10))
+      Rails.logger.info "Sending invite email to #{self.email} with token #{self.invite_token}"
+      VendorMailer.invite_email(self).deliver_now
+      Rails.logger.info "Invite email sent successfully to #{self.email}"
+    rescue => e
+      Rails.logger.error "Failed to send invite email to #{self.email}: #{e.message}"
+      Rails.logger.error e.backtrace.join("\n")
+      raise e
     end
+  end
 end
