6f4ff22dc65a3fa805d0a15c7a4b4f5ed4cedf7d
umarsabir <umarsabir1997@gmail.com>
2025-10-03 06:03:10 +0500

    Add debugging to vendor invite token validation

--- app/controllers/vendors/registrations_controller.rb
+++ app/controllers/vendors/registrations_controller.rb
@@ -34,12 +34,23 @@ class Vendors::RegistrationsController <  Devise::RegistrationsController
 
   def check_invite_token
     if params[:token].present?
+      Rails.logger.info "Checking invite token: #{params[:token]}"
       @invited_vendor = InvitedVendor.find_by(invite_token: params[:token])
+      Rails.logger.info "Found invited vendor: #{@invited_vendor.inspect}"
+      
+      if @invited_vendor.present?
+        Rails.logger.info "Token expires at: #{@invited_vendor.expires_at}"
+        Rails.logger.info "Current time: #{Time.current}"
+        Rails.logger.info "Token valid: #{@invited_vendor.expires_at > Time.current}"
+      end
+      
       unless @invited_vendor.present? && @invited_vendor.expires_at > Time.current
+        Rails.logger.error "Invalid or expired invite token: #{params[:token]}"
         redirect_to new_vendor_session_path, alert: "Invalid or expired invite token"
       end
     else
       # No token provided - redirect to login
+      Rails.logger.error "No token provided for vendor registration"
       redirect_to new_vendor_session_path, alert: "You need a valid invitation link to register as a vendor"
     end
   end
