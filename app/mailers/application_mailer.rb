cc0e61a0a17c6515f4b097c0ad22135cc6c30c7d
umarsabir <umarsabir1997@gmail.com>
2025-10-03 05:41:39 +0500

    Use verified email mumarsabir97@gmail.com as sender

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: ENV['FROM_EMAIL'] || 'no-reply@wheelsonrent.com'
+  default from: ENV['FROM_EMAIL'] || 'mumarsabir97@gmail.com'
   layout 'mailer'
 end
