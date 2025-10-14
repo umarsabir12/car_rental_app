634291c4ef8aa7d1897656e87bf7965f42ed2f75
Shehroz-94 <shehrozehsan@gmail.com>
2025-10-14 20:06:37 +0500

    minor updates and fixes

---
+++ config/initializers/contants.rb
@@ -0,0 +1,3 @@
+module AppConstants
+  CAR_CATEGORIES = [['Sedan', 'Sedan'], ['SUV', 'SUV'], ['Sports', 'Sports'], ['Luxury', 'Luxury'], ['Electric', 'Electric'], ['Hybrid', 'Hybrid'], ['Van', 'Van'], ['Truck', 'Truck']].freeze
+end
