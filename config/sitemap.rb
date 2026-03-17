865afde8908147e0ddc2f883f91e2de173456fb3
Shehroz-94 <shehrozehsan@gmail.com>
2026-03-17 14:59:09 +0500

    added logs for sitmap config

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -24,31 +24,35 @@ SitemapGenerator::Sitemap.search_engines = {
 }
 
 SitemapGenerator::Sitemap.create do
+  Rails.logger.info "[Sitemap] Starting create block..."
   add root_path, priority: 1.0, changefreq: "daily"
-
-  # Car listing page
   add cars_path, priority: 0.8, changefreq: "daily"
 
-  # Individual car pages
-  Car.find_each do |car|
-    add car_path(car),
-        lastmod:     car.updated_at,
-        changefreq:  "daily",
-        priority:    0.8
+  car_count = 0
+  Car.with_approved_mulkiya.available.find_each do |car|
+    begin
+      add car_path(car), lastmod: car.updated_at, changefreq: "daily", priority: 0.8
+      car_count += 1
+    rescue => e
+      Rails.logger.error "[Sitemap] Error adding car #{car.id}: #{e.message}"
+    end
   end
+  Rails.logger.info "[Sitemap] Added #{car_count} cars."
 
-  # Blog listing page
   add blogs_path, priority: 0.7, changefreq: "daily"
 
-  # Individual blog posts (published only)
+  blog_count = 0
   Blog.published.find_each do |blog|
-    add blog_path(blog),
-        lastmod:     blog.updated_at,
-        changefreq:  "weekly",
-        priority:    0.7
+    begin
+      add blog_path(blog), lastmod: blog.updated_at, changefreq: "weekly", priority: 0.7
+      blog_count += 1
+    rescue => e
+      Rails.logger.error "[Sitemap] Error adding blog #{blog.id}: #{e.message}"
+    end
   end
+  Rails.logger.info "[Sitemap] Added #{blog_count} blogs."
 
-  # Static pages
-  add "/terms_of_use",                         priority: 0.4
-  add "/list-your-car-rental-marketplace",     priority: 0.6
+  add "/terms_of_use", priority: 0.4
+  add "/list-your-car-rental-marketplace", priority: 0.6
+  Rails.logger.info "[Sitemap] Creation block finished."
 end
