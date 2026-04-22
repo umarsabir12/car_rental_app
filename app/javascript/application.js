44e3a83881832a317075bb0c93a558653830756e
Shehroz-94 <shehrozehsan@gmail.com>
2026-04-22 16:56:17 +0500

    fixed mallfunctionaling booking modal

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,8 +2,7 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import flatpickr from "flatpickr"
 
-// Make it available globally for inline scripts and helper functions
-window.flatpickr = flatpickr.default || flatpickr;
+window.flatpickr = flatpickr;
 
 console.log('🚀 application.js: flatpickr loaded');
 
